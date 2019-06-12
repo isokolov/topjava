1d683d767f067760f777172f12480719ba9a6210
Illya Sokolov <illya.sokolov82@gmail.com>
2019-06-12 15:11:50 +0200

    HW0 solution with Stream API

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -19,7 +20,8 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
         );
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+        List<UserMealWithExceed> filteredWithExceeded = getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+        filteredWithExceeded.forEach(System.out::println);
 //        .toLocalDate();
 //        .toLocalTime();
     }
@@ -29,7 +31,18 @@ public class UserMealsUtil {
         // TODO return filtered list with correctly exceeded field
         //System.out.println("TODO return filtered list with correctly exceeded field");
 
-        /* creating a HashMap for different days with an ArrayList of daily meals */
+        Map<LocalTime, Integer> coloriesSumByDay = mealList.stream()
+                .collect(Collectors.groupingBy(um -> um.getDateTime().toLocalTime(), Collectors.summingInt(UserMeal::getCalories)));
+
+        return mealList.stream().filter(um -> TimeUtil.isBetween(um.getDateTime().toLocalTime(), startTime, endTime))
+                .map(um -> new UserMealWithExceed(um.getDateTime(), um.getDescription(), um.getCalories(),
+                        coloriesSumByDay.get(um.getDateTime().toLocalDate()) > caloriesPerDay))
+                .collect(Collectors.toList());
+
+
+
+        /* Solution without Stream API
+        // creating a HashMap for different days with an ArrayList of daily meals /
         Map<LocalDate, List<UserMeal>> map = new HashMap<>();
         for (UserMeal userMeal : mealList) {
             LocalDate date = userMeal.getDateTime().toLocalDate();
@@ -46,16 +59,16 @@ public class UserMealsUtil {
             }
         }
 
-        /* ArrayList for meals with exceeded */
+        // ArrayList for meals with exceeded /
         List<UserMealWithExceed> exceedList = new ArrayList<>();
 
-        /* Creating a filtered list only for the meals between 2 dates */
+        // Creating a filtered list only for the meals between 2 dates /
         for (Map.Entry<LocalDate, List<UserMeal>> pair : map.entrySet()) {
             List<UserMeal> firstDayMeals = pair.getValue();
             //break;
             int countColoriesPerDay = 0;
 
-            /* check if day usage exceeded that all meals are added to exceeded List */
+            // check if day usage exceeded that all meals are added to exceeded List /
             for (UserMeal userMeal : firstDayMeals) {
                 countColoriesPerDay += userMeal.getCalories();
             }
@@ -66,13 +79,7 @@ public class UserMealsUtil {
                     }
                 }
             }
-        }
+        }*/
 
-        /* testing toString() to see the results */
-        for (UserMealWithExceed userMealWithExceed : exceedList) {
-            System.out.println(userMealWithExceed);
-        }
-
-        return exceedList;
     }
 }
