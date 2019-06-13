8b7d63d6ba6a9d6da0177bc4ef1c53f706009d21
Illya Sokolov <illya.sokolov82@gmail.com>
2019-06-13 18:12:18 +0200

    HW1 Working with Servlets

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,11 +1,20 @@
 package ru.javawebinar.topjava.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 
 public class UserServlet extends javax.servlet.http.HttpServlet {
 
+    private static final Logger log = LoggerFactory.getLogger(UserServlet.class);
+
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        //request.getRequestDispatcher("/users.jsp").forward(request, response);
+        log.debug("redirect to users");
+
+//        request.getRequestDispatcher("/users.jsp").forward(request, response);
         response.sendRedirect("users.jsp");
+        log.debug("forward to users");
+        request.getRequestDispatcher("/users.jsp").forward(request, response);
     }
 }
